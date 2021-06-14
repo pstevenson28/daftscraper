2c42e4526dad6669a613fb575ccdfb9c7338a394
pstevenson28 <pearsestevenson@gmail.com>
2021-06-14 09:55:54 +0000

    CCT location

--- scraper.py
+++ scraper.py
@@ -1,14 +1,11 @@
-from daftlistings import Daft
+from daftlistings import Daft, Location, SearchType
 
- daft = Daft()
- listings = daft.search()
+daft = Daft()
+daft.set_location(Location.COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN)
+daft.set_search_type(SearchType.STUDENT_ACCOMMODATION)
 
- daft = Daft()
- daft.set_location(Location.COLLEGE_OF_COMPUTING_AND_TECHNOLOGU_DUBLIN)
- daft.set_search_type(SearchType.STUDENT_ACCOMMODATION)
-
- listings = daft.search()
- daft.search(max_pages=1)
+listings = daft.search()
+daft.search(max_pages=1)
 
 for listing in listings:
     print(listing.title)
