287ee1da7b619a9ce08b4f3158b7a9100663b2d3
pstevenson28 <pearsestevenson@gmail.com>
2021-06-14 11:20:35 +0000

    code comments

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 from daftlistings import Daft, Location, SearchType
 
 daft = Daft()
-daft.set_location(Location.COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN)
+daft.set_location(Location.COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN) #Search for student accomodation near Dundalk IT.
 daft.set_search_type(SearchType.STUDENT_ACCOMMODATION)
 daft.set_min_price(400)
 daft.set_max_price(700)
 
+#lists only 1 page from daft
 listings = daft.search(max_pages=1)
+#inputting the coordinates from google maps and using the lambda function gives us accommodation nearest CCT
 COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN_coords = [53.34611743880528, -6.258371046939863]
 listings.sort(key=lambda x: x.distance_to(COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN_coords))
 
@@ -14,5 +16,5 @@ for listing in listings:
     print(listing.title)
     print(listing.price)
     print(listing.daft_link)
-    print(f'{listing.distance_to(COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN_coords):.3}km')
+    print(f'{listing.distance_to(COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN_coords):.3}km') #Search properties according to criteria then sort by nearness to Dublin Castle
 
