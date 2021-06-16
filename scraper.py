b95f8ad81362a0ac6f3138190b028c2cd9385ce8
pstevenson28 <pearsestevenson@gmail.com>
2021-06-16 15:10:06 +0000

    Change in search type

--- scraper.py
+++ scraper.py
@@ -1,6 +1,22 @@
 import time
 # from daftlistings import Daft, Location, SearchType
 
+from daftlistings import Daft, Location, SearchType, PropertyType
+
+daft = Daft()
+daft.set_location(Location.DUBLIN)
+daft.set_search_type(SearchType.RESIDENTIAL_SALE)
+daft.set_property_type(PropertyType.HOUSE)
+daft.set_min_price(400000)
+daft.set_max_price(500000)
+
+listings = daft.search()
+
+for listing in listings:
+    print(listing.title)
+    print(listing.price)
+    print(listing.daft_link)
+
 # daft = Daft()
 # daft.set_location(Location.COLLEGE_OF_COMPUTING_TECHNOLOGY_DUBLIN) #Search for student accomodation near Dundalk IT.
 # daft.set_search_type(SearchType.STUDENT_ACCOMMODATION)
